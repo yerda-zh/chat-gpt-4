49eba9e401d730064e46a2d3e6698b2079182576
yerda-zh <yerdaulet.04@gmail.com>
2023-07-04 23:40:36 +0500

    worked on cta & possibility section

--- src/components/cta/cta.component.jsx
+++ src/components/cta/cta.component.jsx
@@ -1,8 +1,16 @@
-import './cta.styles.jsx';
+import './cta.styles.css';
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className="gpt4__cta">
+      <div className="gpt4__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt4__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/possibility.container.jsx
+++ src/containers/possibility/possibility.container.jsx
@@ -1,9 +1,27 @@
-import './possibility.styles.jsx';
+import './possibility.styles.css';
+import possibilityImage from '../../assets/robot.png';
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
-  )
+    <div className="gpt4__possibility section__padding" id="possibility">
+      <div className="gpt4__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt4__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Roughly 44 percent of companies are looking to make serious
+          investments in AI and integrate it into their businesses. And of the
+          9,130 patents received by IBM inventors in 2021, 2,300 were
+          AI-related.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Possibility;
