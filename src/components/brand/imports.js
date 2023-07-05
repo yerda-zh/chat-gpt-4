d30f8f8b14c0e508c459de5b8bc257228807fdde
yerda-zh <yerdaulet.04@gmail.com>
2023-07-05 16:31:14 +0500

    Updates

--- src/components/brand/imports.js
+++
@@ -1,7 +0,0 @@
-import google from '../../assets/google.png';
-import atlassian from '../../assets/atlassian.png';
-import shopify from '../../assets/shopify.png';
-import slack from '../../assets/slack.png';
-import dropbox from '../../assets/dropbox.png';
-
-export {google, atlassian, shopify, slack, dropbox};
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Navbar} from './navbar/navbar.component';
-export {default as Article} from './article/article.component';
-export {default as Brand} from './brand/brand.component';
-export {default as Cta} from './cta/cta.component';
-export {default as Feature} from './feature/feature.component';
